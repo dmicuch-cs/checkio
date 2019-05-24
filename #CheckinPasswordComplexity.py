42dbabcdf325249fb11593ff3a9f8ca0994af78b
Dusan Micuch <dusan.micuch@leafacademy.eu>
2019-05-24 11:44:29 +0200

    CheckinPasswordComplexity.py

---
+++ #CheckinPasswordComplexity.py
@@ -0,0 +1,36 @@
+#Checkin password complexity
+
+import re
+def checkio(data: str) -> bool:
+
+    CLow=False
+    CUp=False
+    CNum=False
+    #replace this for solution
+    if len(data)>9:
+        for c in data:
+            if re.match("[a-z]", c):
+                CLow=True
+            elif re.match("[A-Z]", c):
+                CUp=True
+            elif re.match("[0-9]", c):
+                CNum=True
+    if  CLow & CUp & CNum:
+        return True 
+    else:
+        return False
+
+#Some hints
+#Just check all conditions
+
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert checkio('A1213pokl') == False, "1st example"
+    assert checkio('bAse730onE4') == True, "2nd example"
+    assert checkio('asasasasasasasaas') == False, "3rd example"
+    assert checkio('QWERTYqwerty') == False, "4th example"
+    assert checkio('123456123456') == False, "5th example"
+    assert checkio('QwErTy911poqqqq') == True, "6th example"
+    print("Coding complete? Click 'Check' to review your tests and earn cool rewards!")
+        
